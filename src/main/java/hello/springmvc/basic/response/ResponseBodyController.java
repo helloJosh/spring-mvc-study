79b7b634ed10021d7dd7d31e81ba7aa2195b3948
Kim ByungWoo <kimjoshua135@gmail.com>
2023-12-19 22:43:01 +0900

    Resposne, Request Http added

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,10 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+}
